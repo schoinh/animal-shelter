923407f3b836a7e8f7bfc78f675888a524b69c06
Erik Irgens and Na Hyung Choi <na@>
2019-08-06 15:20:18 -0700

    Add a way to sort the animals by name alphabetical

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -3,6 +3,7 @@ using AnimalShelter.Models;
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace AnimalShelter.Controllers
 {
     public class AnimalsController : Controller
@@ -16,6 +17,7 @@ namespace AnimalShelter.Controllers
 
         public ActionResult Index()
         {
+            // Using LINQ to order our list of animals by ascending alphaetical order of name (model.Name)
             List<Animal> model = _db.Animals.ToList();
             var query =
                 from animal in model
@@ -23,6 +25,19 @@ namespace AnimalShelter.Controllers
                 select animal;
             return View(query.ToList());
         }
+
+        // // IndexSort()  allows for user input to further refine results via their chosen species.
+        // public ActionResult IndexSort(string species)
+        // {
+
+        //     List<Animal> model = _db.Animals.ToList();
+        //     var query =
+        //         from animal in model
+        //         where animal.Species == species
+        //         orderby animal.Name ascending
+        //         select animal;
+        //     return View(query.ToList());
+        // }
 
         public ActionResult Create()
         {
